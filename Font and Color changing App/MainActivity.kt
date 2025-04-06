67a772226d04330ba50ef4f088ca2f65b2390188
Chandru AIML-A <231501031@rajalakshmi.edu.in>
2025-04-06 21:27:21 +0530

    Update MainActivity.kt

--- Font and Color changing App/MainActivity.kt
+++ Font and Color changing App/MainActivity.kt
@@ -1,77 +1,69 @@
-<?xml version="1.0" encoding="utf-8"?>
-<LinearLayout xmlns:android="http://schemas.android.com/apk/res/android"
-    xmlns:app="http://schemas.android.com/apk/res-auto"
-    xmlns:tools="http://schemas.android.com/tools"
-    android:id="@+id/main"
-    android:layout_width="match_parent"
-    android:layout_height="match_parent"
-    tools:context=".MainActivity"
-    android:orientation="vertical">
-    <RelativeLayout
-        android:layout_width="wrap_content"
-        android:layout_height="wrap_content">
-        <EditText
-            android:id="@+id/textview"
-            android:layout_width="match_parent"
-            android:layout_height="wrap_content"
-            android:hint="Enter your name"
-            android:padding="12dp"
-            android:inputType="textPersonName"
-            android:layout_marginTop="150dp"
-            />
+package com.example.eventlistener
 
-        <Button
-            android:id="@+id/ok"
-            android:layout_width="80dp"
-            android:layout_height="40dp"
-            android:background="@drawable/rounded_button"
-            android:text="OK"
-            android:layout_marginLeft="150dp"
-            android:layout_marginTop="230dp"
-            android:textSize="15dp"
-            android:onClick="text" />
-    </RelativeLayout>
-    <RelativeLayout
-        android:layout_width="match_parent"
-        android:layout_height="wrap_content">
-        <TextView
-            android:layout_width="match_parent"
-            android:layout_height="wrap_content"
-            android:id="@+id/text"
-            android:hint="Text"
-            android:layout_marginTop="50dp"
-            android:textSize="25dp"
-            android:layout_marginLeft="170dp">
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 
-        </TextView>
-    </RelativeLayout>
-    <RelativeLayout
-        android:layout_width="match_parent"
-        android:layout_height="wrap_content">
-        <Button
-            android:id="@+id/font"
-            android:layout_width="140dp"
-            android:layout_height="50dp"
-            android:background="@drawable/rounded_button"
-            android:text="Change font"
-            android:layout_marginLeft="50dp"
-            android:layout_marginTop="30dp"
-            android:textSize="15dp"
-            android:onClick="font"
-            >
-        </Button>
-        <Button
-            android:id="@+id/color"
-            android:layout_width="140dp"
-            android:layout_height="50dp"
-            android:background="@drawable/rounded_button"
-            android:text="Change color"
-            android:layout_marginLeft="20dp"
-            android:layout_marginTop="30dp"
-            android:textSize="15dp"
-            android:layout_toRightOf="@+id/font"
-            android:onClick="color"
-            >
-        </Button>
-    </RelativeLayout>
-</LinearLayout>
+
+class MainActivity : AppCompatActivity() {
+    var count=0;
+    val colorIds = listOf(
+        R.color.red,
+        R.color.green,
+        R.color.blue,
+        R.color.orange,
+        R.color.purple,
+        R.color.lightblue
+        )
+    var count1=0;
+    val fonts= listOf(
+        R.font.font,
+        R.font.font1
+    )
+    lateinit var old_font:android.graphics.Typeface;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val text: TextView = findViewById(R.id.text);
+        old_font=text.typeface;
+    }
+
+    fun font(view: View) {
+
+        val text: TextView = findViewById(R.id.text);
+        val newFont=ResourcesCompat.getFont(this,fonts[count1%fonts.size])
+        text.typeface = newFont;
+        count1++
+        Toast.makeText(getApplicationContext(), "Font Changed", Toast.LENGTH_SHORT).show();
+    }
+    fun color(view: View) {
+        val text: TextView = findViewById(R.id.text);
+        val color= ContextCompat.getColor(this,colorIds[count%colorIds.size]);
+        text.setTextColor(color);
+        count++;
+        Toast.makeText(getApplicationContext(), "Color Changed", Toast.LENGTH_SHORT).show();
+
+    }
+    fun text(view: View) {
+        val textView: TextView = findViewById(R.id.textview);
+        val output: String=textView.text.toString();
+        textView.setText("");
+        val text: TextView = findViewById(R.id.text);
+        text.setText(output);
+        text.typeface=old_font;
+
+    }
+}
